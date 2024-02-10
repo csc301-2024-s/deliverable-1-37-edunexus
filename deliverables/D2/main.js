24b718a1c1eff24e35ec4614f44d609f8cf4b511
jaffar <keikei.jaffar@mail.utoronto.ca>
2024-02-10 05:13:24 -0500

    Update Electron main process for window management improvements

---
+++ deliverables/D2/main.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+
+let mainWindow;
+
+console.log("Starting app...");
+function createWindow () {
+  console.log("Creating main window...");
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  mainWindow.loadFile('login.html');
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
