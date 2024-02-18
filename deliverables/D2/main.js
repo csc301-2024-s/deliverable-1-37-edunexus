04d22f76eb6b2cf9b92a894c33b95b5ce0646c12
Kenneth <chif.chan@mail.utoronto.ca>
2024-02-18 00:31:17 -0500

    Finish student and teacher

--- deliverables/D2/main.js
+++ deliverables/D2/main.js
@@ -1,40 +1,39 @@
 const { app, BrowserWindow, Menu, ipcMain } = require('electron/main')
 const path = require('node:path')
-const database = require('./database.js')
+const database = require('./database/database.js')
 
 // Users
 async function checkUsername (event, username) {
   const webContents = event.sender
   try {
-    const existingUser = await database.getUserbyUsername(username)
-    console.log(existingUser)
+    const existingUser = await database.getUser(username)
     if (existingUser) {
       webContents.send('userResponse', [true, 'Username found'])
-      return { success: true };
+      return { success: true }
     }
     webContents.send('userResponse', [false, 'Username not found'])
-    return { success: false, error: 'Username not found' };
+    return { success: false, error: 'Username not found' }
   } catch (error) {
     webContents.send('userResponse', [false, 'Error: ' + error.message])
-    return { success: false, error: error.message };
+    return { success: false, error: error.message }
   }
 }
 
 async function createUser (event, username, password) {
   const webContents = event.sender
   try {
-    const existingUser = await database.getUserbyUsername(username);
+    const existingUser = await database.getUser(username)
     if (existingUser) {
       webContents.send('userResponse', [false, 'Username already in use'])
-      return { success: false, error: 'Username already in use' };
+      return { success: false, error: 'Username already in use' }
     }
 
-    const userId = await database.insertUser(username, password);
+    const userId = await database.insertUser(username, password)
     webContents.send('userResponse', [true, 'User successfully created with id ' + userId])
-    return { success: true, userId };
+    return { success: true, userId }
   } catch (error) {
     webContents.send('userResponse', [false, 'Error: ' + error.message])
-    return { success: false, error: error.message };
+    return { success: false, error: error.message }
   }
 }
 
@@ -44,13 +43,45 @@ async function login (event, username, password) {
     const existingUser = await database.checkLogin(username, password)
     if (existingUser) {
       webContents.send('userResponse', [true, 'Login successful'])
-      return { success: true };
+      return { success: true }
     }
     webContents.send('userResponse', [false, 'Username or password is incorrect'])
-    return { success: false, error: 'Username not found' };
+    return { success: false, error: 'Username not found' }
   } catch (error) {
     webContents.send('userResponse', [false, 'Error: ' + error.message])
-    return { success: false, error: error.message };
+    return { success: false, error: error.message }
+  }
+}
+
+async function deleteUser (event, username) {
+  const webContents = event.sender
+  try {
+    const deletedUser = await database.deleteUser(username)
+    if (deletedUser) {
+      webContents.send('userResponse', [true, 'Delete successful'])
+      return { success: true }
+    }
+    webContents.send('userResponse', [false, 'Username not found'])
+    return { success: false, error: 'Username not found' }
+  } catch (error) {
+    webContents.send('userResponse', [false, 'Error: ' + error.message])
+    return { success: false, error: error.message }
+  }
+}
+
+async function updateUserPassword (event, username, password) {
+  const webContents = event.sender
+  try {
+    const changedUser = await database.updateUserPassword(username, password)
+    if (changedUser) {
+      webContents.send('userResponse', [true, 'Password changed'])
+      return { success: true }
+    }
+    webContents.send('userResponse', [false, 'Username not found, password changed failed'])
+    return { success: false, error: 'Username not found' }
+  } catch (error) {
+    webContents.send('userResponse', [false, 'Error: ' + error.message])
+    return { success: false, error: error.message }
   }
 }
 
@@ -58,76 +89,156 @@ async function login (event, username, password) {
 async function createStudent (event, studentName, studentNumber, age) {
   const webContents = event.sender
   try {
-    const existingStudent = await database.getStudentbyStudentNumber(studentNumber);
+    const existingStudent = await database.getStudent(studentNumber)
     if (existingStudent) {
       webContents.send('studentResponse', [false, 'Student number already in use'])
-      return { success: false, error: 'Student number already in use' };
+      return { success: false, error: 'Student number already in use' }
     }
 
-    const studentID = await database.insertStudent(studentName, studentNumber, age);
+    const studentID = await database.insertStudent(studentName, studentNumber, age)
     webContents.send('studentResponse', [true, 'Student successfully created with id ' + studentID])
-    return { success: true, studentID };
+    return { success: true, studentID }
   } catch (error) {
     webContents.send('studentResponse', [false, 'Error: ' + error.message])
-    return { success: false, error: error.message };
+    return { success: false, error: error.message }
   }
 }
 
 async function checkStudentNumber (event, studentNumber) {
   const webContents = event.sender
   try {
-    const existingStudent = await database.getStudentbyStudentNumber(studentNumber)
+    const existingStudent = await database.getStudent(studentNumber)
     if (existingStudent) {
       webContents.send('studentResponse', [true, 'student id: ' + existingStudent.id +
-      '\nstudent name: ' + existingStudent.name +
-      '\nstudent number: ' + existingStudent.student_number +
-      '\nstudent age: ' + existingStudent.age])
-      return { success: true };
+      ', student name: ' + existingStudent.name +
+      ', student number: ' + existingStudent.student_number +
+      ', student age: ' + existingStudent.age])
+      return { success: true }
     }
     webContents.send('studentResponse', [false, 'Student not found'])
-    return { success: false, error: 'Student not found' };
+    return { success: false, error: 'Student not found' }
   } catch (error) {
     webContents.send('studentResponse', [false, 'Error: ' + error.message])
-    return { success: false, error: error.message };
+    return { success: false, error: error.message }
   }
 }
 
+async function getAllStudent (event) {
+  const webContents = event.sender
+  try {
+    const existingStudents = await database.getAllStudent()
+    if (existingStudents) {
+      s = ""
+      existingStudents.forEach((student) => {
+        s += 'student id: ' + student.id
+        s += ', student name: ' + student.name
+        s += ', student number: ' + student.student_number
+        s += ', student age: ' + student.age
+        s += '\n'
+      })
+      webContents.send('studentResponse', [true, s])
+      return { success: true }
+    }
+    webContents.send('studentResponse', [false, 'No student found'])
+    return { success: false, error: 'No student found' }
+  } catch (error) {
+    webContents.send('studentResponse', [false, 'Error: ' + error.message])
+    return { success: false, error: error.message }
+  }
+}
+
+async function deleteStudent (event, studentNumber) {
+  const webContents = event.sender
+  try {
+    const deletedStudent = await database.deleteStudent(studentNumber)
+    if (deletedStudent) {
+      webContents.send('studentResponse', [true, 'Delete successful'])
+      return { success: true }
+    }
+    webContents.send('studentResponse', [false, 'Student number not found'])
+    return { success: false, error: 'Studentname not found' }
+  } catch (error) {
+    webContents.send('studentResponse', [false, 'Error: ' + error.message])
+    return { success: false, error: error.message }
+  }
+}
+
+// Teachers
 async function createTeacher (event, teacherName, teacherNumber) {
   const webContents = event.sender
   try {
-    const existingTeacher = await database.getTeacherbyTeacherNumber(teacherNumber);
+    const existingTeacher = await database.getTeacher(teacherNumber)
     if (existingTeacher) {
       webContents.send('teacherResponse', [false, 'Teacher number already in use'])
-      return { success: false, error: 'Teacher number already in use' };
+      return { success: false, error: 'Teacher number already in use' }
     }
 
-    const teacherId = await database.insertTeacher(teacherName, teacherNumber);
+    const teacherId = await database.insertTeacher(teacherName, teacherNumber)
     webContents.send('teacherResponse', [true, 'Teacher successfully created with id ' + teacherId])
-    return { success: true, teacherId };
+    return { success: true, teacherId }
   } catch (error) {
     webContents.send('teacherResponse', [false, 'Error: ' + error.message])
-    return { success: false, error: error.message };
+    return { success: false, error: error.message }
   }
 }
 
 async function checkTeacherNumber (event, teacherNumber) {
   const webContents = event.sender
   try {
-    const existingTeacher = await database.getTeacherbyTeacherNumber(teacherNumber)
+    const existingTeacher = await database.getTeacher(teacherNumber)
     if (existingTeacher) {
-      console.log(existingTeacher)
       webContents.send('teacherResponse', [true, 'teacher id: ' + existingTeacher.id +
       '\nteacher name: ' + existingTeacher.name +
       '\nteacher number: ' + existingTeacher.teacher_number])
-      return { success: true };
+      return { success: true }
     }
     webContents.send('teacherResponse', [false, 'Teacher not found'])
-    return { success: false, error: 'Teacher not found' };
+    return { success: false, error: 'Teacher not found' }
   } catch (error) {
     webContents.send('teacherResponse', [false, 'Error: ' + error.message])
-    return { success: false, error: error.message };
+    return { success: false, error: error.message }
   }
 }
+
+async function getAllTeacher (event) {
+  const webContents = event.sender
+  try {
+    const existingTeachers = await database.getAllTeacher()
+    if (existingTeachers) {
+      s = ""
+      existingTeachers.forEach((teacher) => {
+        s += 'teacher id: ' + teacher.id
+        s += ', teacher name: ' + teacher.name
+        s += ', teacher number: ' + teacher.teacher_number
+        s += '\n'
+      })
+      webContents.send('teacherResponse', [true, s])
+      return { success: true }
+    }Students
+    webContents.send('teacherResponse', [false, 'No teacher found'])
+    return { success: false, error: 'No teacher found' }
+  } catch (error) {
+    webContents.send('teacherResponse', [false, 'Error: ' + error.message])
+    return { success: false, error: error.message }
+  }
+}
+
+async function deleteTeacher (event, teacherNumber) {
+  const webContents = event.sender
+  try {
+    const deletedTeacher = await database.deleteTeacher(teacherNumber)
+    if (deletedTeacher) {
+      webContents.send('teacherResponse', [true, 'Delete successful'])
+      return { success: true }
+    }
+    webContents.send('teacherResponse', [false, 'Teacher number not found'])
+    return { success: false, error: 'Teachername not found' }
+  } catch (error) {
+    webContents.send('teacherResponse', [false, 'Error: ' + error.message])
+    return { success: false, error: error.message }
+  }
+}
+
 
 function createWindow () {
   const mainWindow = new BrowserWindow({
@@ -144,9 +255,18 @@ app.whenReady().then(() => {
   ipcMain.on('createUser', createUser)
   ipcMain.on('checkUsername', checkUsername)
   ipcMain.on('login', login)
+  ipcMain.on('deleteUser', deleteUser)
+  ipcMain.on('updatePassword', updateUserPassword)
   // Students
   ipcMain.on('createStudent', createStudent)
   ipcMain.on('checkStudentNumber', checkStudentNumber)
+  ipcMain.on('getAllStudent', getAllStudent)
+  ipcMain.on('deleteStudent', deleteStudent)
+  // Teachers
+  ipcMain.on('createTeacher', createTeacher)
+  ipcMain.on('checkTeacherNumber', checkTeacherNumber)
+  ipcMain.on('getAllTeacher', getAllTeacher)
+  ipcMain.on('deleteTeacher', deleteTeacher)
 
   createWindow()
   app.on('activate', function () {
--- deliverables/D2/renderer.js
+++ deliverables/D2/renderer.js
@@ -2,6 +2,8 @@
 const createUser = document.getElementById('createUser')
 const checkUser = document.getElementById('checkUser')
 const login = document.getElementById('login')
+const deleteUser = document.getElementById('deleteUser')
+const updatePassword = document.getElementById('updatePassword')
 const usernameElement = document.getElementById('username')
 const passwordElement = document.getElementById('password')
 const userResponseElement = document.getElementById('userResponse')
@@ -23,6 +25,17 @@ login.addEventListener('click', async() => {
   window.api.login(username, password)
 })
   
+deleteUser.addEventListener('click', async() => {
+  const username = usernameElement.value
+  window.api.deleteUser(username)
+})
+
+updatePassword.addEventListener('click', async() => {
+  const username = usernameElement.value
+  const password = passwordElement.value
+  window.api.updatePassword(username, password)
+})
+
 const counter = document.getElementById('counter')
 
 window.api.userResponse((data) => {
@@ -39,6 +52,8 @@ window.api.userResponse((data) => {
 // Student part
 const createStudent = document.getElementById('createStudent')
 const checkStudentNumber = document.getElementById('checkStudentNumber')
+const getAllStudent = document.getElementById('getAllStudent')
+const deleteStudent = document.getElementById('deleteStudent')
 const studentResponseElement = document.getElementById('studentResponse')
 const studentNameElement = document.getElementById('studentName')
 const studentNumberElement = document.getElementById('studentNumber')
@@ -57,6 +72,15 @@ checkStudentNumber.addEventListener('click', async() => {
   window.api.checkStudentNumber(studentNumber)
 })
 
+getAllStudent.addEventListener('click', async() => {
+  window.api.getAllStudent()
+})
+
+deleteStudent.addEventListener('click', async() => {
+  const studentNumber = studentNumberElement.value
+  window.api.deleteStudent(studentNumber)
+})
+
 window.api.studentResponse((data) => {
   success = data[0]
   response = data[1]
@@ -65,5 +89,47 @@ window.api.studentResponse((data) => {
     studentResponseElement.style.color = 'green'
   } else {
     studentResponseElement.style.color = 'red'
+  }
+})
+
+// Teacher part
+const createTeacher = document.getElementById('createTeacher')
+const checkTeacherNumber = document.getElementById('checkTeacherNumber')
+const getAllTeacher = document.getElementById('getAllTeacher')
+const deleteTeacher = document.getElementById('deleteTeacher')
+const teacherResponseElement = document.getElementById('teacherResponse')
+const teacherNameElement = document.getElementById('teacherName')
+const teacherNumberElement = document.getElementById('teacherNumber')
+
+
+createTeacher.addEventListener('click', () => {
+  const teacherName = teacherNameElement.value
+  const teacherNumber = teacherNumberElement.value
+  const age = ageElement.value
+  window.api.createTeacher(teacherName, teacherNumber, age)
+})
+
+checkTeacherNumber.addEventListener('click', async() => {
+  const teacherNumber = teacherNumberElement.value
+  window.api.checkTeacherNumber(teacherNumber)
+})
+
+getAllTeacher.addEventListener('click', async() => {
+  window.api.getAllTeacher()
+})
+
+deleteTeacher.addEventListener('click', async() => {
+  const teacherNumber = teacherNumberElement.value
+  window.api.deleteTeacher(teacherNumber)
+})
+
+window.api.teacherResponse((data) => {
+  success = data[0]
+  response = data[1]
+  teacherResponseElement.innerText = response
+  if (success) {
+    teacherResponseElement.style.color = 'green'
+  } else {
+    teacherResponseElement.style.color = 'red'
   }
 })
