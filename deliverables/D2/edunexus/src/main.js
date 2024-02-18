df29d497eec81c1a5bb7e663f41ac2d421c9a9f8
Arthur Gao <arthurgao@Steamboat.localdomain>
2024-02-18 02:52:50 -0500

    Add eslint. Fix all ESLint Errors

--- deliverables/D2/edunexus/src/main.js
+++ deliverables/D2/edunexus/src/main.js
@@ -1,30 +1,32 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
+// const path = require('path');
 
-const isDev = !app.isPackaged
+const isDev = !app.isPackaged;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
-  app.quit();
+    app.quit();
 }
 
 const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
-    },
-  });
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            // eslint-disable-next-line no-undef
+            preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
+        },
+    });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+    // and load the index.html of the app.
+    // eslint-disable-next-line no-undef
+    mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
-  // Open the DevTools.
-  if (isDev) {
-    mainWindow.webContents.openDevTools();
-  }
+    // Open the DevTools.
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
 
 };
 
@@ -33,31 +35,31 @@ const createWindow = () => {
 // Some APIs can only be used after this event occurs.
 // app.on('ready', createWindow);
 app.whenReady().then(() => {
-  ipcMain.on('ping-pong', handlePing)
-  createWindow()
-})
+    ipcMain.on('ping-pong', handlePing);
+    createWindow();
+});
 
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
 
 function handlePing () {
-  console.log("React pinged the main process!")
+    console.log('React pinged the main process!');
 }
