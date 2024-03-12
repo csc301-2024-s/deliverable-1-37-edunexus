4b8d54ef07f4b679203446c89af8ac3d3e3c90d6
Kevin Shen <kevinzshen3@gmail.com>
2024-03-12 17:33:27 -0400

    Update preload.js for login authentication

--- src/preload.js
+++ src/preload.js
@@ -7,13 +7,13 @@ contextBridge.exposeInMainWorld('api', {
     // pingPong: (title) => ipcRenderer.send('ping-pong', title)
 
     send: (channel, data) => {
-        let validChannels = ['loginData', 'signupData', 'request-report-generation'];
+        let validChannels = ['loginData', 'signupData', 'request-report-generation', 'login-authentication'];
         if (validChannels.includes(channel)) {
             ipcRenderer.send(channel, data);
         }
     },
     receive: (channel, func) => {
-        let validChannels = ['loginResponse', 'signupResponse', 'report-generation-complete', 'report-generation-failed'];
+        let validChannels = ['loginResponse', 'signupResponse', 'report-generation-complete', 'report-generation-failed', 'login-failed', 'login-success'];
         if (validChannels.includes(channel)) {
             ipcRenderer.on(channel, (event, ...args) => func(...args));
         }
