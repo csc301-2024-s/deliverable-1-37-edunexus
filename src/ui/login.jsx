5a5044dcf836db37d5269666b9795b6ae1ce292d
Oswin Gan <oswin.gan@gmail.com>
2024-03-11 21:00:16 -0400

    Added style to the login page

--- src/ui/login.jsx
+++ src/ui/login.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app.jsx';
-
+import { TextField, Button, Container, Typography, Box } from '@mui/material';
+import SchoolIcon from '@mui/icons-material/School'; 
 
 function Login() {
     const [username, setUsername] = useState('');
@@ -27,29 +28,64 @@ function Login() {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="username">Username:</label>
-                <input
-                    required
-                    type="text"
-                    id="username"
-                    value={username}
-                    onChange={handleUsernameChange}
-                />
-            </div>
-            <div>
-                <label htmlFor="password">Password:</label>
-                <input
-                    required
-                    type="password"
-                    id="password"
-                    value={password}
-                    onChange={handlePasswordChange}
-                />
-            </div>
-            <button type="submit">Login</button>
-        </form>
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                minHeight: '100vh',
+            }}
+        >
+            <Typography variant='h2' fontWeight='bold' mb={2} color='#222831'>
+                EduNexus
+                {' '}
+                <SchoolIcon sx={{ color: '#222831', fontSize: '4rem' }}/>
+            </Typography>
+            <Container component='main' maxWidth='xs'>
+                <div>
+                    
+                    <Typography component='h1' variant='h5' color='#222831'>
+                        Sign in
+                    </Typography>
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            margin='normal'
+                            required
+                            fullWidth
+                            id='username'
+                            label='Username'
+                            name='username'
+                            autoComplete='username'
+                            autoFocus
+                            value={username}
+                            onChange={handleUsernameChange}
+                        />
+                        <TextField
+                            margin='normal'
+                            required
+                            fullWidth
+                            name='password'
+                            label='Password'
+                            type='password'
+                            id='password'
+                            autoComplete='current-password'
+                            value={password}
+                            onChange={handlePasswordChange}
+                        />
+                        <Button
+                            type='submit'
+                            fullWidth
+                            variant='contained'
+                            sx={{ mt: 3, mb: 2 }}
+                            style={{ backgroundColor: '#76ABAE', color: '#FFFFFF' }}
+                        >
+                            Sign In
+                        </Button>
+                    </form>
+                </div>
+            </Container>
+        </Box>
     );
 }
 
