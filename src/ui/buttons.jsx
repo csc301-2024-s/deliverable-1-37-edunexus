cb110e5a331c583fa110651f7e0dc45310cedc58
Arthur Gao <arthurerconggao@gmail.com>
2024-03-21 14:24:49 -0400

    Add JSDoc on some button and dashboard jsx

--- src/ui/buttons.jsx
+++ src/ui/buttons.jsx
@@ -3,7 +3,14 @@ import Button from '@mui/material/Button';
 import '@emotion/styled';
 import './styles.css'; 
 
-
+/**
+ * `Buttons` is a React component that renders a set of buttons for uploading spreadsheets,
+ * generating PDF reports, and visualizing class statistics. It also manages the state
+ * for showing a popup component that provides instructions for uploading a spreadsheet.
+ *
+ * @returns {React.JSX.Element} The `Buttons` component renders three buttons with different functionalities
+ * and a `PopupComponent` if the `showPopup` state is true.
+ */
 function Buttons() {
     const [showPopup, setShowPopup] = useState(false);
 
@@ -35,6 +42,15 @@ function Buttons() {
     );
 }
 
+/**
+ * `PopupComponent` is a React component that displays a modal popup with instructions
+ * for uploading a spreadsheet. It includes an input for file upload and a close button
+ * to dismiss the popup.
+ *
+ * @param {Object} props - The props object for the `PopupComponent`.
+ * @param {Function} props.onClose - A function to call when the popup should be closed.
+ * @returns {React.JSX.Element} The `PopupComponent` renders a modal with instructions and a file input.
+ */
 function PopupComponent({ onClose }) {
     return (
         <div className="popup">
